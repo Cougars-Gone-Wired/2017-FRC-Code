6ccd7c5211cc8304954a3b8d04252de1a7eee047
Team2996 <Team2996@LAPTOP-S55OS4PO>
2017-01-21 15:47:04 -0700

    BoolDoub
    BoolDoub

---
+++ Command-Based-Robot/src/org/usfirst/frc/team2996/robot/boolDoub.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team2996.robot;
+
+public class boolDoub {
+	boolean bool;
+	double doub;
+	boolDoub(boolean bool, double doub){
+		this.bool = bool;
+		this.doub = doub;
+	}
+}
