b67dc84d0944351421b9f0d807fa6a8cce1dbde5
Team2996 <Team2996@LAPTOP-S55OS4PO.cssd11.k12.co.us>
2017-02-15 19:12:16 -0700

    jdskfkjf
    dsf
    fds

--- Command-Based-Robot/src/org/usfirst/frc/team2996/robot/GripPipelineListener.java
+++
@@ -1,8 +0,0 @@
-//package org.usfirst.frc.team2996.robot;
-//
-//public class GripPipelineListener implements VisionRunner.Listener<GripPipeline>{
-//
-//	public void copyPipelineOutputs(GripPipeline gripPipeline){
-//		gripPipeline.filterContoursOutput();
-//	}
-//}
